07057f3b4cd947ac974e33b537dfeba753f468bb
Tony Justin Briza <brizatonyjustin@gmail.com>
2025-07-12 15:39:44 +0800

    Remove unused state variables for existing components and component models in PCBuildAdvisor

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
 export { default as PCBuildAdvisor } from './PCBuildAdvisor'
 export { default as BudgetInput } from './BudgetInput'
-export { default as ComponentSelector } from './ComponentSelector'
 export { default as BuildRecommendation } from './BuildRecommendation'
